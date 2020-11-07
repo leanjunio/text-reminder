31845a479ec4724db6984a30bd72dfc26b73ecd5
Lean Junio <leanjunio@live.com>
2020-11-07 15:00:14 -0500

    Start creating tests for user model

--- src/__tests__/user.spec.ts
+++ src/__tests__/user.spec.ts
@@ -1,5 +1,19 @@
-import User from '../models/User';
+import { IUser, UserModel } from '../models/User';
+
+import db from '../db-setup';
 
 describe('User model', () => {
-  test('Should be able to create a User model', () => {});
+  beforeAll(async () => await db.connect());
+  afterEach(async () => await db.clearDatabase());
+  afterAll(async () => await db.closeDatabase());
+
+  test('Should be able to create a User model', async () => {
+    const sampleUser = {
+      firstName: 'John',
+      mobile: '+16478394645',
+      password: 'some-password',
+    };
+
+    const user = await UserModel.create(sampleUser);
+  });
 });
