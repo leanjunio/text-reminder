a87fd8c6aadcd52dab48977aaf32afeb3d8a2644
Lean Junio <leanjunio@live.com>
2020-11-07 15:30:10 -0500

    Refactor variable names

--- src/__tests__/helpers.spec.ts
+++ src/__tests__/helpers.spec.ts
@@ -3,7 +3,10 @@ import { hashPassword } from '../helpers/password';
 describe('Password', () => {
   test('Should be able to encrypt a password string', async () => {
     const password: string = 'some-password';
-    const hashedpassword = await hashPassword(password);
+    const hashedPassword = await hashPassword(password);
+
+    expect(hashedPassword).toEqual(expect.not.stringMatching(password));
+  });
 
     expect(hashedpassword).toEqual(expect.not.stringMatching(password));
   });
