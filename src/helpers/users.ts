c097d11861dfba7076e0364971c9f3c5b152eb3e
Lean Junio <leanjunio@live.com>
2020-11-07 23:01:53 -0500

    Create helper for updateUserData

--- src/helpers/users.ts
+++ src/helpers/users.ts
@@ -10,3 +10,19 @@ export const getUserData = async (userId: Types.ObjectId) => {
   const user = await UserModel.findById(userId).exec();
   return user ?? `No user with id: ${userId} found`;
 };
+
+export const updateUserData = async (userId: Types.ObjectId, data: {}) => {
+  const options = { new: true };
+  const update = { ...data };
+
+  const updatedUser = await UserModel.findByIdAndUpdate(userId, update, options).exec();
+
+  return updatedUser;
+};
+
+export const fetchAllUserReminders = async (userId: Types.ObjectId) => {
+  const projections = { reminders: true };
+  const user = await UserModel.findById(userId, projections).exec();
+
+  return user ?? `User has no reminders.`;
+};
