e44310aa18222a3a4edff066e06457deac0bee88
Lean Junio <leanjunio@live.com>
2020-11-07 19:05:00 -0500

    Create getUserData helper

--- src/helpers/users.ts
+++ src/helpers/users.ts
@@ -1,6 +1,12 @@
+import { Types } from 'mongoose';
 import { UserModel, IUser } from '../models/User';
 
 export const registerUser = async (data: IUser) => {
   const createdUser = await UserModel.create(data);
   return createdUser;
+};
+
+export const getUserData = async (userId: Types.ObjectId) => {
+  const user = await UserModel.findById(userId).exec();
+  return user ?? `No user with id: ${userId} found`;
 };
