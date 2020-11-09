26856ac282adbfa8bf7333ea9235bfa326dfac95
Lean Junio <leanjunio@live.com>
2020-11-08 21:07:53 -0500

    Create new route for user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,9 @@
 import { Router, Request, Response } from 'express';
+import { registerUser } from '../controllers/user';
 
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {});
+router.post('/register', registerUser);
 
 export default router;
