280d8bc44fac3b561876fb3e98093f867b20ddd3
Lean Junio <leanjunio@live.com>
2020-11-08 21:24:30 -0500

    Fix lint errors

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,8 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import { registerUser } from '../controllers/user';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {});
 router.post('/register', registerUser);
 
 export default router;
