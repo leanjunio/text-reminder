f8ec922307aa32cd7d96137633c29e055542a4d4
Lean Junio <leanjunio@live.com>
2020-11-28 15:43:24 -0500

    Add new gitignore

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import httpStatus from 'http-status-codes';
 import { Application, Request, Response } from 'express';
 
-import userRoutes from './users';
+// import userRoutes from './users';
 import authRoutes from './auth';
 
 import { defaultErrorHandler } from '../controllers/error';
@@ -9,6 +9,6 @@ import { defaultErrorHandler } from '../controllers/error';
 export const routeConfig = (app: Application) => {
   app.get('/api/check', (_: Request, res: Response) => res.status(httpStatus.OK).send('Health check'));
   app.use('/auth', authRoutes);
-  app.use('/users', userRoutes);
+  // app.use('/users', userRoutes);
   app.use(defaultErrorHandler);
 };
