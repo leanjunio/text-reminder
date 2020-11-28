84a52924e406a09d9ca1cf6c2414b2babdb48f50
Lean Junio <leanjunio@live.com>
2020-11-28 15:40:19 -0500

    Create twilio service: - Send verification token

---
+++ src/services/twilio.ts
@@ -0,0 +1,10 @@
+import twilio from 'twilio';
+
+const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN, { lazyLoading: true });
+const TWILIO_SERVICE_SID: string = process.env.TWILIO_SERVICE_SID!;
+
+export function sendVerificationToken(mobile: string) {
+  return client.verify
+    .services(TWILIO_SERVICE_SID)
+    .verifications.create({ to: `+1${mobile}`, channel: 'sms' });
+}
