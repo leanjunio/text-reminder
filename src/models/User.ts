59eda03e9f325708c4564e55b562ed767e426a78
Lean Junio <leanjunio@live.com>
2020-11-27 23:41:18 -0500

    Remove password from user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,7 @@
-import { getModelForClass, prop, Ref, pre } from '@typegoose/typegoose';
+import { getModelForClass, prop, Ref } from '@typegoose/typegoose';
 
 import { ReminderClass } from './Reminder';
-import { hashPassword } from '../utilities/password';
 
-@pre<UserClass>('save', async function () {
-  this.password = await hashPassword(this.password);
-})
 class UserClass {
   @prop()
   public firstName!: string;
@@ -18,9 +14,6 @@ class UserClass {
 
   @prop()
   public mobile!: string;
-
-  @prop()
-  public password!: string;
 
   @prop({ ref: () => ReminderClass })
   public reminders?: Ref<ReminderClass>[];
