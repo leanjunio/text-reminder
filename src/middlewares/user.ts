4112f5ae8f330019c39748ef014f378836415822
Lean Junio <leanjunio@live.com>
2020-11-28 15:17:21 -0500

    Create new user middleware: - Check if user is able to login

--- src/middlewares/user.ts
+++ src/middlewares/user.ts
@@ -2,9 +2,18 @@ import { Request, Response, NextFunction } from 'express';
 
 import * as UserServices from '../services/users';
 
-export async function isAlreadyExistingUser(req: Request, _: Response, next: NextFunction): Promise<void> {
+export async function isAbleToRegister(req: Request, _: Response, next: NextFunction): Promise<void> {
   try {
-    await UserServices.isAlreadyExistingUser(req.body.email);
+    await UserServices.isAbleToRegister(req.body.email);
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function isAbleToLogin(req: Request, _: Response, next: NextFunction): Promise<void> {
+  try {
+    await UserServices.isAbleToLogin(req.body.email);
     next();
   } catch (error) {
     next(error);
