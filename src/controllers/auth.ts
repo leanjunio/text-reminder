3da0058acbf86e51c48883c342369312999d14d7
Lean Junio <leanjunio@live.com>
2020-11-28 16:27:12 -0500

    Add auth controller: - Verify login token

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -18,10 +18,23 @@ export async function registerUser(req: Request, res: Response, next: NextFuncti
 
 export async function sendLoginToken(req: Request, res: Response, next: NextFunction) {
   try {
-    await UserServices.sendLoginTokenToUserMobile(req.body);
+    await UserServices.sendLoginTokenToUserMobile(req.body.email);
 
     res.status(StatusCodes.OK).json({
       message: 'Login token sent. Enter the passcode to login',
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function verifyLoginToken(req: Request, res: Response, next: NextFunction) {
+  try {
+    const token = await UserServices.verifyLoginToken(req.body.token, req.body.mobile);
+
+    res.status(StatusCodes.OK).json({
+      message: 'Token verified. User logged in.',
+      token,
     });
   } catch (error) {
     next(error);
