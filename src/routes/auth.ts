04731c42e8eeef6613ccb4fca1b48e0a3581ae14
Lean Junio <leanjunio@live.com>
2020-11-28 15:17:45 -0500

    Create new route: - Login User

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -8,8 +8,10 @@ const router = Router();
 
 router.post(
   '/register',
-  [UserValidation.registerUser, UserMiddlewares.isAlreadyExistingUser],
+  [UserValidation.registerUser, UserMiddlewares.isAbleToRegister],
   AuthControllers.registerUser
 );
+
+router.post('/login', [UserValidation.loginUser, UserMiddlewares.isAbleToLogin], AuthControllers.loginUser);
 
 export default router;
