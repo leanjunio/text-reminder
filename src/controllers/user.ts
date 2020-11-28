603c0c77dfab8e94a9fdcf688dce8a43bafc48c6
Lean Junio <leanjunio@live.com>
2020-11-28 15:02:20 -0500

    Cleanup

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,21 +1,21 @@
 import { Request, Response } from 'express';
-import * as userHelpers from '../helpers/users';
+import * as userHelpers from '../services/users';
 
 export const registerUser = async (req: Request, res: Response) => {
   const { mobile } = req.body;
-  const foundUser = await userHelpers.checkIfUserAlreadyExists(mobile);
+  // const foundUser = await userHelpers.checkIfUserAlreadyExists(mobile);
 
-  if (!foundUser) {
-    const registeredUser = await userHelpers.registerUser({ ...req.body });
+  // if (!foundUser) {
+  //   const registeredUser = await userHelpers.registerUser({ ...req.body });
 
-    res.json({
-      message: 'User has been registered!',
-      data: { user: registeredUser },
-    });
-  } else {
-    res.json({
-      message: 'User already exists',
-      data: { user: foundUser },
-    });
-  }
+  //   res.json({
+  //     message: 'User has been registered!',
+  //     data: { user: registeredUser },
+  //   });
+  // } else {
+  //   res.json({
+  //     message: 'User already exists',
+  //     data: { user: foundUser },
+  //   });
+  // }
 };
