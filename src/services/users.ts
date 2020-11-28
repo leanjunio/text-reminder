a6ecc9fe20c7f46be6016cf8cf4224f640065674
Lean Junio <leanjunio@live.com>
2020-11-28 16:39:32 -0500

    Refactor

--- src/services/users.ts
+++ src/services/users.ts
@@ -52,43 +52,9 @@ export async function verifyLoginToken(code: string, mobile: string) {
     throw error;
   }
 }
-
-function findUserWithEmail(email: string) {
+export function findUserWithEmail(email: string) {
   return UserModel.findOne({ email }).exec();
 }
-
-const getUserData = async (userId: Types.ObjectId) => {
-  const user = await UserModel.findById(userId).exec();
-  return user ?? `No user with id: ${userId} found`;
-};
-
-const updateUserData = async (userId: Types.ObjectId, data: {}) => {
-  const options = { new: true };
-  const update = { ...data };
-
-  const updatedUser = await UserModel.findByIdAndUpdate(userId, update, options).exec();
-
-  return updatedUser;
-};
-
-const fetchAllUserReminders = async (userId: Types.ObjectId) => {
-  const projections = { reminders: true };
-  const user = await UserModel.findById(userId, projections).exec();
-
-  return user?.reminders ?? `User has no reminders.`;
-};
-
-const appendReminderToUser = async (userId: Types.ObjectId, reminderId: Types.ObjectId) => {
-  const user = await UserModel.findById(userId).exec();
-
-  const reminders = user?.reminders;
-  reminders?.push(reminderId);
-
-  const update = { reminders };
-  const updatedUser = await updateUserData(userId, update);
-
-  return updatedUser;
-};
 
 export async function isAbleToRegister(email: string): Promise<boolean> {
   try {
