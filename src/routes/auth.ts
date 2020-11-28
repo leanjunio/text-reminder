0d47f879ce46bea0d56be54f12f010f5e1e7b34e
Lean Junio <leanjunio@live.com>
2020-11-28 15:02:00 -0500

    Add user register route: - Use body validation - Check if user exists - Use AuthController.registerUser

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 
-import { registerUser } from '../controllers/user';
+import * as AuthControllers from '../controllers/auth';
+import * as UserMiddlewares from '../middlewares/user';
+import UserValidation from '../middlewares/validations/user';
 
 const router = Router();
 
-router.post('/register', registerUser);
+router.post(
+  '/register',
+  [UserValidation.registerUser, UserMiddlewares.isAlreadyExistingUser],
+  AuthControllers.registerUser
+);
 
 export default router;
