4fc1185f6c8a290143de211502f9163d12682f6a
Lean Junio <leanjunio@live.com>
2020-11-28 15:01:21 -0500

    Add authRoutes and default error handler route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,13 @@ import httpStatus from 'http-status-codes';
 import { Application, Request, Response } from 'express';
 
 import userRoutes from './users';
+import authRoutes from './auth';
+
+import { defaultErrorHandler } from '../controllers/error';
 
 export const routeConfig = (app: Application) => {
   app.get('/api/check', (_: Request, res: Response) => res.status(httpStatus.OK).send('Health check'));
+  app.use('/auth', authRoutes);
   app.use('/users', userRoutes);
+  app.use(defaultErrorHandler);
 };
