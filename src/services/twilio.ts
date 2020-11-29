ee09c610fed20ac27faf5f1144f0a5dd6e0c6811
Lean Junio <leanjunio@live.com>
2020-11-29 13:45:43 -0500

    Add service for sending message with body

--- src/services/twilio.ts
+++ src/services/twilio.ts
@@ -1,14 +1,21 @@
 import twilio from 'twilio';
 
-const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN, { lazyLoading: true });
-const TWILIO_SERVICE_SID: string = process.env.TWILIO_SERVICE_SID!;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const serviceSid: string = process.env.TWILIO_SERVICE_SID!;
+
+const fromNumber = process.env.FROM_NUMBER;
+
+const client = twilio(accountSid, authToken, { lazyLoading: true });
 
 export function sendVerificationToken(mobile: string) {
-  return client.verify
-    .services(TWILIO_SERVICE_SID)
-    .verifications.create({ to: `+1${mobile}`, channel: 'sms' });
+  return client.verify.services(serviceSid).verifications.create({ to: `+1${mobile}`, channel: 'sms' });
 }
 
 export function retrieveVerificationStatus(code: string, mobile: string) {
-  return client.verify.services(TWILIO_SERVICE_SID).verificationChecks.create({ to: `+1${mobile}`, code });
+  return client.verify.services(serviceSid).verificationChecks.create({ to: `+1${mobile}`, code });
+}
+
+export function sendMessage(mobile: string, message: string) {
+  return client.messages.create({ body: message, from: fromNumber, to: `+1${mobile}` });
 }
