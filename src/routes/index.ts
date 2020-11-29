69f97adcc307243cb92f37fa0f1de295ce7a6a83
Lean Junio <leanjunio@live.com>
2020-11-28 19:52:16 -0500

    Add reminder to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,14 @@
 import httpStatus from 'http-status-codes';
 import { Application, Request, Response } from 'express';
 
-// import userRoutes from './users';
 import authRoutes from './auth';
+import reminderRoutes from './reminder';
 
 import { defaultErrorHandler } from '../controllers/error';
 
 export const routeConfig = (app: Application) => {
   app.get('/api/check', (_: Request, res: Response) => res.status(httpStatus.OK).send('Health check'));
   app.use('/auth', authRoutes);
-  // app.use('/users', userRoutes);
+  app.use('/reminder', reminderRoutes);
   app.use(defaultErrorHandler);
 };
