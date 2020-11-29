4770920d73a0209a93e948d9737e6ce7f52ba554
Lean Junio <leanjunio@live.com>
2020-11-29 11:15:21 -0500

    Create reminder service: - add reminder under user with matching email

--- src/services/reminder.ts
+++ src/services/reminder.ts
@@ -1,3 +1,29 @@
-import { Reminder, IReminder } from '../models/Reminder';
+import { ReminderModel, IReminder } from '../models/Reminder';
+import * as UserServices from '../services/users';
+import { config } from '../config';
 
-export function addReminderUnderUserWithEmail(email: string, reminder: IReminder) {}
+const log = config.LOG;
+
+export async function addReminderUnderUserWithMatchingEmail(email: string, reminder: IReminder) {
+  try {
+    const userWithMatchingEmail = await UserServices.findUserWithMatchingEmail(email);
+
+    if (!userWithMatchingEmail) {
+      return Promise.reject(new Error(`Cannot add reminder. User with email ${email} cannot be found.`));
+    }
+
+    const newReminder = createReminder(reminder);
+
+    userWithMatchingEmail.reminders?.push(newReminder._id);
+    userWithMatchingEmail.save();
+
+    return newReminder.save();
+  } catch (error) {
+    log.error(error.message);
+    throw error;
+  }
+}
+
+function createReminder(reminder: IReminder) {
+  return new ReminderModel(reminder);
+}
