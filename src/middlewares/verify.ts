9bc5a7bc2218c9f7266850d01ad9251fbe00d22f
Lean Junio <leanjunio@live.com>
2020-11-28 20:32:16 -0500

    Add check if current user is logged in

---
+++ src/middlewares/verify.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+import * as UserServices from '../services/users';
+
+interface IDecodedTokenData extends Object {
+  data?: {
+    mobile?: string;
+  };
+}
+
+export async function isCurrentUserLoggedIn(req: Request, _: Response, next: NextFunction) {
+  const { email } = req.params;
+
+  if (!email) {
+    next(new Error(`Cannot check if current user of email ${email} is logged in.`));
+  }
+
+  const token = extractTokenFromHeader(req.headers);
+  const decoded: IDecodedTokenData = jwt.verify(token, process.env.SECRET_KEY!);
+
+  const mobileFromToken = decoded.data?.mobile;
+
+  try {
+    const foundUserMobile = await UserServices.retrieveUserMobileFromEmail(email);
+    const areMatchingMobiles = foundUserMobile === mobileFromToken;
+
+    if (!areMatchingMobiles) {
+      next(new Error(`You do not have access to this resource. Please re-check your credentials`));
+    }
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
+function extractTokenFromHeader(headers: any) {
+  const authHeader = headers.authorization;
+  return authHeader && authHeader.split(' ')[1];
+}
