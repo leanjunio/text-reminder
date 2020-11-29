095f1913c0cd9c2971bcf484645f28bcc37d44c2
Lean Junio <leanjunio@live.com>
2020-11-29 14:41:21 -0500

    Run cron jobs

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import bodyParser from 'body-parser';
 import { routeConfig } from './routes/index';
 import * as db from './db';
 
+import * as CronServices from './services/cron';
+
 const app: Application = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,6 +13,10 @@ app.use(bodyParser.json());
 
 routeConfig(app);
 db.connect();
+
+(async () => {
+  await CronServices.sendAllRemindersToUsers();
+})();
 
 export default {
   listen: (port: number | string) => app.listen(port, () => console.log(`Running on port: ${port}`)),
